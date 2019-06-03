17b467878b06b98c0fa6babb0d6257687fbcadd5
fer <ferp@protonmail.ch>
2019-06-03 11:32:19 +0200

    added dyamic updating

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -5,28 +5,46 @@ import Nav from './Nav.js'
 
 class Beers extends React.Component {
   state = {
-    beers : []
+    beers : [],
+    onScrollEnd: false,
+    page: 1 
   }
 
   componentDidMount() {
     let service = axios.create({
-      baseURL: 'http://localhost:3005'
+      baseURL: 'https://api.punkapi.com/v2/'
     })
     service.get(`/beers`)
       .then((result)=>{
-        console.log(result)
-        this.setState({beers: result.data}, () => console.log(this.state.beers.slice( 0,25 )))})
+        this.setState({beers: result.data} )})
+  }
+
+  componentDidUpdate() {
+    let service = axios.create({
+      baseURL: 'https://api.punkapi.com/v2/'
+    })
+
+    this.state.onScrollEnd &&
+    service.get(`/beers?page=${this.state.page}`)
+      .then ((result) => {
+       const newBeers = [...this.state.beers, ...result.data];
+        this.setState({beers: newBeers, onScrollEnd: false});
+      });
   }
 
   render() {
+
+    const test =  (window.pageYOffset + window.innerHeight >= document.documentElement.scrollHeight - 500);
+
+
     return(
       <Fragment>
         <Nav/>
         <h1>Beers!</h1>
 
       <div className="beer-grid">
-        { this.state.beers.length && 
-            this.state.beers.slice(0,25).map((beer, index) => (
+        { this.state.beers.length ? 
+            this.state.beers.map((beer, index) => (
               <div className="beer-item" key={index}>
                 <div className="img-container">
                   <img src={beer.image_url} alt={beer.name}/>
@@ -35,10 +53,11 @@ class Beers extends React.Component {
                 <p>{beer.tagline}</p>
               </div>
             ))
+          : null
         
         }
+      <h1 onClick={() => this.setState({onScrollEnd: true})}>more Beer!</h1>
       </div>
-
       </Fragment>
     )
   }
